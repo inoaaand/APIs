084d236abbff66d847a8e8ca163ec07cd75146bc
fernanda <20233005420@estudantes.ifpr.edu.br>
2025-06-23 10:51:16 -0300

    Implementações consumindo uma API

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-function carregarPersonagens() {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then(response => response.json())
-      .then(data => {
-        const container = document.getElementById("character-list");
-        data.results.forEach(personagem => {
-          container.innerHTML += `
-            <div class="character">
-              <img src="${personagem.image}" alt="${personagem.name}" width="100">
-              <div>
-                <h2>${personagem.name}</h2>
-                <p>Status: ${personagem.status}</p>
-                <p>Espécie: ${personagem.species}</p>
-                <p>Origem: ${personagem.origin.name}</p>
-              </div>
+async function carregarPersonagens() {
+    try {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await response.json();
+  
+      const container = document.getElementById("character-list");
+      data.results.forEach(personagem => {
+        container.innerHTML += `
+          <div class="character">
+            <img src="${personagem.image}" alt="${personagem.name}" width="100">
+            <div>
+              <h2>${personagem.name}</h2>
+              <p>Status: ${personagem.status}</p>
+              <p>Espécie: ${personagem.species}</p>
+              <p>Origem: ${personagem.origin.name}</p>
             </div>
-          `;
-        });
-      })
-      .catch(error => {
-        console.error("Erro ao carregar personagens:", error);
-        document.getElementById("character-list").innerText =
-          "Erro ao carregar dados.";
+          </div>
+        `;
       });
+    } catch (err) {
+      console.error("Erro ao carregar personagens:", err);
+      document.getElementById("character-list").innerText = "Erro ao carregar dados.";
+    }
   }
   
   carregarPersonagens();
